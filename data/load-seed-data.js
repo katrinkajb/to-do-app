1c14983d509106c1bba3ccd642a78b7c3b64e0cf
KB <katrinkajb@gmail.com>
2021-03-01 14:46:47 -0800

    some file changes

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const todos = require('./todos.js');
+const todosData = require('./todos.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,7 +25,7 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      todos.map(todo => {
+      todosData.map(todo => {
         return client.query(`
                     INSERT INTO todos (todo, completed, owner_id)
                     VALUES ($1, $2, $3);
